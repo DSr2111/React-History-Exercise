b0c143f99dcf31d8f61adbd728a21268eebb2ef7
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-09-25 08:01:44 -0400

    rest of code refactor

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Joke from "./Joke";
-import "./JokeList.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Joke from './Joke';
+import './JokeList.css';
 
 function JokeList({ numJokesToGet = 5 }) {
   const [jokes, setJokes] = useState([]);
@@ -9,84 +9,75 @@ function JokeList({ numJokesToGet = 5 }) {
 
   /* get jokes if there are no jokes */
 
-  useEffect(function () {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { ...jokeObj } = res.data;
+  useEffect(
+    function () {
+      async function getJokes() {
+        let j = [...jokes];
+        let seenJokes = new Set();
+        try {
+          while (j.length < numJokesToGet) {
+            let res = await axios.get('https://icanhazdadjoke.com', {
+              headers: { Accept: 'application/json' },
+            });
+            let { ...jokeObj } = res.data;
 
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
+            if (!seenJokes.has(jokeObj.id)) {
+              seenJokes.add(jokeObj.id);
+              j.push({ ...jokeObj, votes: 0 });
+            } else {
+              console.error('duplicate found!');
+            }
           }
+          setJokes(j);
+          setIsLoading(false);
+        } catch (err) {
+          console.error(err);
         }
-        setJokes(j);
-        setIsLoading(false)
-      } catch (err) {
-        console.error(err);
       }
-    }
 
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
+      if (jokes.length === 0) getJokes();
+    },
+    [jokes, numJokesToGet]
+  );
 
   /* empty joke list, set to loading state, and then call getJokes */
 
-  generateNewJokes() {
-    this.setState({ isLoading: true});
-    this.getJokes();
+  function generateNewJokes() {
+    setJokes([]);
+    setIsLoading(true);
   }
 
   /* change vote for this id by delta (+1 or -1) */
 
-  vote(id, delta) {
-    this.setState(st => ({
-      jokes: st.jokes.map(j =>
-        j.id === id ? { ...j, votes: j.votes + delta } : j
-      )
-    }));
+  function vote(id, delta) {
+    setJokes((allJokes) =>
+      allJokes.map((j) => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+    );
   }
 
   /* render: either loading spinner or list of sorted jokes. */
 
-  render() {
-    let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
-    if (this.state.isLoading) {
-      return (
-        <div className="loading">
-          <i className="fas fa-4x fa-spinner fa-spin" />
-        </div>
-      )
-    }
-
+  if (isLoading) {
     return (
-      <div className="JokeList">
-        <button
-          className="JokeList-getmore"
-          onClick={this.generateNewJokes}
-        >
-          Get New Jokes
-        </button>
-
-        {sortedJokes.map(j => (
-          <Joke
-            text={j.joke}
-            key={j.id}
-            id={j.id}
-            votes={j.votes}
-            vote={this.vote}
-          />
-        ))}
+      <div className="loading">
+        <i className="fas fa-4x fa-spinner fa-spin" />
       </div>
     );
   }
+
+  let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div className="JokeList">
+      <button className="JokeList-getmore" onClick={generateNewJokes}>
+        Get New Jokes
+      </button>
+
+      {sortedJokes.map(({ joke, id, votes }) => (
+        <Joke text={joke} key={id} id={id} votes={votes} vote={vote} />
+      ))}
+    </div>
+  );
 }
 
 export default JokeList;
